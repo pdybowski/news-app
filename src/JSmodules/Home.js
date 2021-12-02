94d00639cb19923737e0f577075f705d40566af5
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-02 15:39:57 +0100

    WIP with date and hour

--- src/JSmodules/Home.js
+++ src/JSmodules/Home.js
@@ -1,5 +1,6 @@
 export default class Home {
     constructor() {
+        this.textElement = document.querySelector('.view__textContent')
         this.fullDate = new Date();
         this.day = String(fullDate.getDate()).padStart(2, '0');
         this.year = fullDate.getFullYear();
@@ -7,5 +8,13 @@ export default class Home {
         this.outputDate = `Dziś jest ${day}.${year}r. '\n' Godzina ${hour}`
     }
 
-    
+    start() {
+        setInterval(() => {
+            this.textElement.innerText = outputDate
+        }, 1000)
+    }
+
+    test(){
+        console.log(`${this.fullDate}`)
+    }
 }
