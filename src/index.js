7d49c6c3d8e2795a7972798c45cb37dd74eef7e9
p.dybowski13 <p.dybowski13@gmail.com>
2021-12-04 22:27:14 +0100

    small fixes to file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import { Home }from './JSmodules/Home.js'
-
+import './content/styles/index.css';
+import { Home } from './modules';
 
 // The following two lines of code are responsible for updating the date and time on the home page
-const homePageUpdate = new Home()
-
+const homePageUpdate = new Home();
--- src/modules/home/home.module.js
+++ src/modules/home/home.module.js
@@ -3,38 +3,38 @@ export class Home {
         this.viewElement = document.querySelector('#main');
 
         this.start();
-    };
+    }
 
     start() {
-        this._createContent()
-        this._timer()
-    };
+        this._createContent();
+        this._timer();
+    }
 
     _createContent() {
         this.mainContainer = document.createElement('div');
         this.header = document.createElement('h1');
 
-        this.mainContainer.classList.add("view", "container-lg");
-        this.header.classList.add("view__header", "view__text--color", "h1");
-        this.header.innerText = "Cześć 👋🏼 Dobrze Cię widzieć.";
+        this.mainContainer.classList.add('view', 'container-lg');
+        this.header.classList.add('view__header', 'view__text--color', 'h1');
+        this.header.innerText = 'Cześć 👋🏼 Dobrze Cię widzieć.';
 
         this.mainContainer.appendChild(this.header);
         this.viewElement.appendChild(this.mainContainer);
     }
 
-    _createTimeText(){
+    _createTimeText() {
         this.content = document.createElement('p');
-        this.content.classList.add("view__textContent", "view__text--color", "display-6");
+        this.content.classList.add('view__textContent', 'view__text--color', 'display-6');
         this.mainContainer.appendChild(this.content);
     }
 
     _timer() {
-        this._createTimeText()
+        this._createTimeText();
 
         setInterval(() => {
             this.fullDate = new Date();
             this.day = String(this.fullDate.getDate()).padStart(2, '0');
-            this.month = String(this.fullDate.getMonth()+1).padStart(2, '0');
+            this.month = String(this.fullDate.getMonth() + 1).padStart(2, '0');
             this.year = this.fullDate.getFullYear();
             this.hour = String(this.fullDate.getHours()).padStart(2, '0');
             this.minutes = String(this.fullDate.getMinutes()).padStart(2, '0');
@@ -42,4 +42,4 @@ export class Home {
             this.content.innerText = `Dziś jest ${this.day}.${this.month}.${this.year}r. \n Godzina ${this.hour}:${this.minutes}:${this.sec}`;
         }, 1000);
     }
-};
+}
