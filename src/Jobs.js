569c4f5dc291468e489451332e7761307da432b1
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-04 14:38:58 +0100

    Created Element to display info about the job offer

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,10 +1,13 @@
 export class Jobs {
     constructor(){
+        this.viewElement = document.querySelector('.view')
         this.APP_ID = 07300533;
         this.APP_KEY = 'fadda830981b47b9ef3e023f82b349ca';
         this.URL = `https://api.adzuna.com/v1/api/jobs/pl/search/1?app_id=${this.APP_ID}&app_key=${this.APP_KEY}`;
         this.fetchedData;
+    }
 
+    start(){
         try{
             fetchingData(this.URL);
             this.fetchedData = data
@@ -22,6 +25,17 @@ export class Jobs {
           });
         const data = await response.json()
         return data
+    }
+
+    _createJobItem(){
+        const itemContainer = document.createElement('div');
+        const itemHeader = document.createElement('h4');
+        const additionalInfo = document.createElement('span');
+        const shortDescription = document.createElement('p');
+
+        itemContainer.appendChild(itemHeader);
+        itemContainer.appendChild(additionalInfo);
+        itemContainer.appendChild(shortDescription);
     }
 }
 
