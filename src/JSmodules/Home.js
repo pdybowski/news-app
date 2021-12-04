8448c34b2d727a4952a2e00ed5f945296640f987
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-04 14:53:26 +0100

    Added .padStart method to variables

--- src/JSmodules/Home.js
+++ src/JSmodules/Home.js
@@ -1,24 +1,41 @@
 export class Home {
     constructor() {
-        this.textElement = document.querySelector('.view__textContent');
-    
+        this.viewElement = document.querySelector('#main');
+
         this.start();
     };
 
     start() {
-       this._timer()
+        this._createContent()
+        this._timer()
     };
+
+    _createContent() {
+        const mainContainer = document.createElement('div');
+        const header = document.createElement('h1');
+        const content = document.createElement('p');
+
+        this.mainContainer.className.add("view container-lg");
+        this.header.className.add("view__header view__text--color h1");
+        this.content.className.add("view__textContent view__text--color display-6");
+
+        this.viewElement.appendChild(mainContainer);
+        this.viewElement.appendChild(header);
+        this.viewElement.appendChild(content);
+
+        return this.mainContainer;
+    }
 
     _timer() {
         setInterval(() => {
             this.fullDate = new Date();
             this.day = String(this.fullDate.getDate()).padStart(2, '0');
-            this.month = this.fullDate.getMonth();
+            this.month = String(this.fullDate.getMonth()).padStart(2, '0');
             this.year = this.fullDate.getFullYear();
-            this.hour = this.fullDate.getUTCHours();
-            this.minutes = this.fullDate.getMinutes();
-            this.sec = this.fullDate.getSeconds();
-            this.textElement.innerText = `Dziś jest ${this.day}.${this.month}.${this.year}r. \n Godzina ${this.hour}:${this.minutes}:${this.sec}`;
+            this.hour = String(this.fullDate.getHours()).padStart(2, '0');
+            this.minutes = String(this.fullDate.getMinutes()).padStart(2, '0');
+            this.sec = String(this.fullDate.getSeconds()).padStart(2, '0');
+            this.mainContainer.innerText = `Dziś jest ${this.day}.${this.month}.${this.year}r. \n Godzina ${this.hour}:${this.minutes}:${this.sec}`;
         }, 1000);
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Home }from './JSmodules/Home.js'
 
+
 // The following two lines of code are responsible for updating the date and time on the home page
 const homePageUpdate = new Home()
 
