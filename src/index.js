072e9b57ca6b7c094d4dec4033b35ebbbff153c5
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-04 14:28:10 +0100

    Removed invoking function in main JavaScript file

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import { Home }from './JSmodules/Home.js'
 
 // The following two lines of code are responsible for updating the date and time on the home page
 const homePageUpdate = new Home()
-homePageUpdate.start()
+
