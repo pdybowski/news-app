ac10c501583f5422abdc5a2d42a778d3323f8762
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-06 10:47:45 +0100

    timer moved to the center

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './content/styles/index.css';
 import { Home } from './modules';
 
-// The following two lines of code are responsible for updating the date and time on the home page
-const homePageUpdate = new Home();
+document.addEventListener('DOMContentLoaded', () => new Home());
+
