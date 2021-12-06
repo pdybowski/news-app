e3de6fb4f4ff10c13408234daf7d066e4eb7d448
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-06 18:25:49 +0100

    Fetching data success

--- src/modules/Jobs/jobs.module.js
+++ src/modules/Jobs/jobs.module.js
@@ -5,20 +5,27 @@ export class Jobs {
         this.APP_KEY = 'fadda830981b47b9ef3e023f82b349ca';
         this.URL = `https://api.adzuna.com/v1/api/jobs/pl/search/1?app_id=${this.APP_ID}&app_key=${this.APP_KEY}`;
         this.fetchedData;
+        this.fetchError;
     }
 
     _render(){
         fetch(this.URL)
         .then(response => response.json())
-        .then(data => console.log(data))
-        // .then(console.log(this.fetchedData))
+        .then(data => this.fetchedData = data.results)
+        .catch(error => this.fetchError = error);
+    
+        this._start(this.fetchedData)
     }
 
     
-    _start(){
+    _start(data){
         this._createJobHeader('h2', 'Job opportunities');
         this._createJobText('Check out the latest jobs in your area!');
         // this._createJobItems();
+
+        for (item of data){
+            return <h1>{item}</h1>
+        }
     }
 
     _createJobItems(title, company, min, max, link){
