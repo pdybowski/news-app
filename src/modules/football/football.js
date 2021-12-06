b3ce1d0bfa878c639ea28698cc7d46755132d828
kamilazajac <kamilaagata.zajac@gmail.com>
2021-12-06 18:38:32 +0100

    button class created, style removed from index.html,put methods inside class

--- src/modules/football/football.js
+++ src/modules/football/football.js
@@ -3,31 +3,30 @@ export class Sport {
         this.mainBox = document.querySelector('#main');
         this.start();
     }
-    start() {
+    _start() {
         this._createContent();
         this._createButtons();
     }
-}
+    _createContent() {
+        this.buttonsContainer = document.createElement('div');
+        this.buttonsContainer.className = 'buttons';
+        this.mainBox.appendChild(buttonsContainer);
+    }
 
-_createContent(){
-    this.buttonsContainer = document.createElement('div');
-    this.buttonsContainer.className = 'buttons';
-    this.mainBox.appendChild(buttonsContainer);
-}
+    _createButtons() {
+        const areaName = document.createElement('button');
+        areaName.className = 'area__name';
+        areaName.innerHTML = 'Europe';
+        buttonsContainer.appendChild(areaName);
 
-_createButtons() {
-    const areaName = document.createElement('button');
-    areaName.className = 'area__name';
-    areaName.innerHTML = 'Europe';
-    buttonsContainer.appendChild(areaName);
+        const leagueName = document.createElement('button');
+        leagueName.className = 'league__name';
+        leagueName.innerHTML = 'Champions League';
+        buttonsContainer.appendChild(leagueName);
 
-    const leagueName = document.createElement('button');
-    leagueName.className = 'league__name';
-    l eagueName.innerHTML = 'Champions League';
-    buttonsContainer.appendChild(leagueName);
-
-    const season = document.createElement('button');
-    season.className = 'area__name';
-    season.innerHTML = '2021/2022';
-    buttonsContainer.appendChild(season);
+        const season = document.createElement('button');
+        season.className = 'area__name';
+        season.innerHTML = '2021/2022';
+        buttonsContainer.appendChild(season);
+    }
 }
