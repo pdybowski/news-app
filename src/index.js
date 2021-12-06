e170a33b08dd02a9880e6fc76374f6eec9157ae4
Patrycja <46846078+patrycjastarzec@users.noreply.github.com>
2021-12-06 19:01:01 +0100

    Added news view (#16)
    * Added news view
    
    * Added changes to the news view
    
    Co-authored-by: p.dybowski13 <p.dybowski13@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ import './content/styles/index.css';
 import { Home } from './modules';
 
 document.addEventListener('DOMContentLoaded', () => new Home());
-
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,1 +1,2 @@
+export * from './news/news.js';
 export * from './home/home.module';
