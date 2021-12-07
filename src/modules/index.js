e07fc96ae48e2faec56c030afaf72d291f7276c7
Justyna Gładysz <61114674+jusgladysz@users.noreply.github.com>
2021-12-08 00:03:34 +0100

    [jg-5] Added basic weather module (#15)
    * Added basic weather module
    
    * Added code review changes to weather module
    
    * Fixed "Uncaught ReferenceError: regeneratorRuntime is not defined" error when use async/await

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,4 @@
 export * from './health/health.js';
 export * from './news/news.js';
 export * from './home/home.module.js';
+export * from './weather/weather.module';
---
+++ src/modules/weather/weather.module.js
@@ -0,0 +1,167 @@
+import api from './weather.api';
+import { createColumn, createRow } from '../../utils';
+
+export class Weather {
+    constructor() {
+        this.viewElement = document.querySelector('#main');
+        this._start();
+    }
+
+    async _start() {
+        try {
+            await this._fetchData();
+        } catch (error) {
+            console.error(error);
+        }
+        this._createContainer();
+    }
+
+    async _fetchData() {
+        this._fiveDayWeather = await api.getFiveDayWeather();
+        this._currentDayWeather = await api.getCurrentDayWeather();
+        this._currentLocationInfo = await api.getCurrentLocationInfo();
+    }
+
+    _createContainer() {
+        this.container = document.createElement('div');
+        this.viewElement.appendChild(this.container);
+        this._createToday();
+        this._createNextDays();
+    }
+
+    _createToday() {
+        const currentDayDiv = createRow();
+        this._createCurrentDayInfoIn(currentDayDiv);
+        this._createCurrentDayWeatherIn(currentDayDiv);
+        this.container.append(currentDayDiv);
+    }
+
+    _createCurrentDayInfoIn(parent) {
+        const currentDayInfoDiv = document.createElement('div');
+        this._createTitleIn(currentDayInfoDiv);
+        this._createInfoContentIn(currentDayInfoDiv);
+        parent.append(currentDayInfoDiv);
+    }
+
+    _createTitleIn(parent) {
+        const title = document.createElement('h1');
+        title.innerText = `Weather in ${this._currentLocationInfo.LocalizedName}`;
+        parent.appendChild(title);
+    }
+
+    _createInfoContentIn(parent) {
+        const infoContentDiv = document.createElement('div');
+        infoContentDiv.innerText = this._fiveDayWeather.Headline.Text;
+        parent.append(infoContentDiv);
+    }
+
+    _createCurrentDayWeatherIn(parent) {
+        const currentDayWeatherDiv = document.createElement('div');
+
+        const dailyForecast = this._fiveDayWeather.DailyForecasts[0];
+        this._createWeatherElementIn(
+            currentDayWeatherDiv,
+            `${this._currentDayWeather[0].Temperature.Metric.Value}°C`,
+            dailyForecast.Day.Icon,
+            `↑ ${dailyForecast.Temperature.Maximum.Value}`,
+            `↓ ${dailyForecast.Temperature.Minimum.Value}`,
+            true
+        );
+
+        parent.append(currentDayWeatherDiv);
+    }
+
+    _createNextDays() {
+        const nextDaysDiv = createRow();
+        this._fiveDayWeather['DailyForecasts'].forEach((dailyData) => {
+            const column = createColumn();
+            this._createWeatherElementIn(
+                column,
+                new Date(dailyData.Date).getDay(),
+                dailyData.Day.Icon,
+                dailyData.Temperature.Maximum.Value,
+                dailyData.Temperature.Minimum.Value
+            );
+            nextDaysDiv.append(column);
+        });
+
+        this.container.append(nextDaysDiv);
+    }
+
+    _createWeatherElementIn(
+        parent,
+        firstElement,
+        icon,
+        maxTemperature,
+        minTemperature,
+        isHorizontal = false
+    ) {
+        const weatherElement = isHorizontal ? createRow() : document.createElement('div');
+
+        weatherElement.append(
+            this._createFirstElementDiv(firstElement, isHorizontal),
+            this._createWeatherIconDiv(icon, isHorizontal),
+            this._createTemperatureRangeDiv(maxTemperature, minTemperature, isHorizontal)
+        );
+
+        parent.append(weatherElement);
+    }
+
+    _createTemperatureRangeDiv(maxTemperature, minTemperature, isHorizontal = false) {
+        const temperatureRangeDiv = isHorizontal ? createColumn() : createRow();
+
+        const maxTempDiv = this._createTempDiv(maxTemperature);
+        maxTempDiv.classList.add('fw-bold');
+
+        temperatureRangeDiv.append(maxTempDiv, this._createTempDiv(minTemperature));
+
+        return temperatureRangeDiv;
+    }
+
+    _createTempDiv(temperature, isHorizontal = false) {
+        const temperatureDiv = isHorizontal ? document.createElement('div') : createColumn();
+        temperatureDiv.innerText = `${temperature}°C`;
+        return temperatureDiv;
+    }
+
+    _createFirstElementDiv(value, isHorizontal = false) {
+        const firstElementDiv = isHorizontal ? createColumn() : document.createElement('div');
+
+        switch (value) {
+            case 0:
+                firstElementDiv.innerText = 'Sun';
+                break;
+            case 1:
+                firstElementDiv.innerText = 'Mon';
+                break;
+            case 2:
+                firstElementDiv.innerText = 'Tue';
+                break;
+            case 3:
+                firstElementDiv.innerText = 'Wed';
+                break;
+            case 4:
+                firstElementDiv.innerText = 'Thu';
+                break;
+            case 5:
+                firstElementDiv.innerText = 'Fri';
+                break;
+            case 6:
+                firstElementDiv.innerText = 'Sat';
+                break;
+            default:
+                firstElementDiv.innerText = value;
+                break;
+        }
+
+        return firstElementDiv;
+    }
+
+    _createWeatherIconDiv(icon, isHorizontal = false) {
+        const weatherIconDiv = isHorizontal ? createColumn() : document.createElement('div');
+        const img = document.createElement('img');
+        img.src = `./content/img/weather/${icon}-s.png`;
+        weatherIconDiv.appendChild(img);
+        return weatherIconDiv;
+    }
+}
