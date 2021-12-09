d472a73d56ba9d952d007c3a9a2ee8f41a5298b4
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-09 15:25:22 +0100

    class .view changed into id #main

--- src/modules/Jobs/jobs.module.js
+++ src/modules/Jobs/jobs.module.js
@@ -1,6 +1,6 @@
 export class Jobs {
     constructor() {
-        this.viewElement = document.querySelector('.view')
+        this.viewElement = document.querySelector('#main')
         this.APP_ID = '07300533';
         this.APP_KEY = 'fadda830981b47b9ef3e023f82b349ca';
         this.URL = `https://api.adzuna.com/v1/api/jobs/pl/search/1?app_id=${this.APP_ID}&app_key=${this.APP_KEY}`;
