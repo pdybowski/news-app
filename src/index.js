b7b5d86baa186c877cb45315eace62f06d81e0ad
kamilazajac <61557939+kamilazajac@users.noreply.github.com>
2021-12-09 01:21:20 +0100

    football init (#14)
    * initial commit
    
    * js changes
    
    * added modules folder
    
    * button class created, style removed from index.html,put methods inside class
    
    * changed class to football_button, start method correction
    
    Co-authored-by: p.dybowski13 <p.dybowski13@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import './content/styles/index.css';
-import { Home } from './modules';
 
-document.addEventListener('DOMContentLoaded', () => new Home());
+document.addEventListener('DOMContentLoaded', () => {});
---
+++ src/modules/football/football.js
@@ -0,0 +1,32 @@
+export class Sport {
+    constructor() {
+        this.mainBox = document.querySelector('#main');
+        this._start();
+    }
+    _start() {
+        this._createContent();
+        this._createButtons();
+    }
+    _createContent() {
+        this.buttonsContainer = document.createElement('div');
+        this.buttonsContainer.className = 'footbal__button';
+        this.mainBox.appendChild(buttonsContainer);
+    }
+
+    _createButtons() {
+        const areaName = document.createElement('button');
+        areaName.className = 'area__name';
+        areaName.innerHTML = 'Europe';
+        buttonsContainer.appendChild(areaName);
+
+        const leagueName = document.createElement('button');
+        leagueName.className = 'league__name';
+        leagueName.innerHTML = 'Champions League';
+        buttonsContainer.appendChild(leagueName);
+
+        const season = document.createElement('button');
+        season.className = 'area__name';
+        season.innerHTML = '2021/2022';
+        buttonsContainer.appendChild(season);
+    }
+}
