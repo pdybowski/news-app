4793e42fcff80fa5f81b9421b01166c7bfcfaedb
Justyna Gładysz <jus.gladysz@gmail.com>
2021-12-11 22:11:27 +0100

    Change weather api key

--- src/modules/weather/weather.config.js
+++ src/modules/weather/weather.config.js
@@ -1,2 +1,2 @@
-export const WEATHER_API_KEY = '7QG1mLbGe9lQFuGHQimBdZszKAnuBETm';
+export const WEATHER_API_KEY = 'vCC75BFjAz8GG8jwDevhm68WZIu3ShpZ';
 export const WEATHER_API_HOST = 'http://dataservice.accuweather.com/';
