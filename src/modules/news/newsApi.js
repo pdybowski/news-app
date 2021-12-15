dd2559f68dc08fc1e5ad6117bf39a12ae3ed9d5f
patrycjastarzec <patryc.sc@gmail.com>
2021-12-15 19:08:56 +0100

    Added newsApi.js module

---
+++ src/modules/news/newsApi.js
@@ -0,0 +1,20 @@
+import { NEWS_API } from '../../shared/environment';
+import { API } from '../../shared/api';
+
+export class NewsApi extends API {
+    constructor() {
+        super(NEWS_API.API_BASE_URL, NEWS_API.API_KEY, null);
+    }
+
+    fetch(query) {
+        return super.fetch(NEWS_API.API_BASE_URL, null, `${query}&apiKey=${NEWS_API.API_KEY}`);
+    }
+
+    getTopNews(header) {
+        return this.fetch(`${header}?language=en&pageSize=5`);
+    }
+
+    getSearchedNews(keyword) {
+        return this.fetch(`everything?language=en&pageSize=5&q=${keyword}`);
+    }
+}
