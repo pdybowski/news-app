a37b1ad519f83dccbee26d0f9c30c816794c97b6
Filip Kaczmarek <f.kaczmarek1@wp.pl>
2021-12-21 17:07:34 +0100

    Created simple dives to display leagues

--- src/modules/football/football.js
+++ src/modules/football/football.js
@@ -3,5 +3,25 @@ export class Sport {
         this.mainBox = document.querySelector('#main');
     }
 
-    
+    _createMainContainer() {
+        this.container = createElement('div', { class: 'football__main-container' });
+        this.mainBox.append(this.container);
+    }
+
+    _createLeaguesContainer() {
+        const leaguesContainer = createElement('div', { class: 'Leagues__container' });
+
+        // loop for leages 
+
+        this.mainBox.append(leaguesContainer);
+    }
+
+    _createLeagueBox(imageSrc, fulllLagueName) {
+        const leagueBox = createElement('div', { class: 'leagues__box' });
+        const logo = createElement('img', { class: 'leagues__box--image', src: imageSrc});
+        const leagueName = createElement('h2', { class: 'leagues__box--name'});
+        leagueName.innerText = fulllLagueName;
+        leagueBox.append(logo, leagueName);
+        this.leaguesContainer.append(leagueBox);
+    }
 }
