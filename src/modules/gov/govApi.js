93301f40ec5364bffb063100bf4ea425ff71c19e
Tomasz <przadkatomasz.alt@gmail.com>
2022-01-04 20:17:22 +0100

    limit api to articles only

--- src/modules/gov/govApi.js
+++ src/modules/gov/govApi.js
@@ -7,17 +7,17 @@ export class GovApi extends API {
         super(GOVERNMENT_API.API_BASE_URL, null, null);
     }
 
-    fetch(query) {
-        return super.fetch(`${GOVERNMENT_API.API_BASE_URL}search?`, null, query);
+    fetch(query, page = 1, per_page = 10) {
+        return super.fetch(
+            `${GOVERNMENT_API.API_BASE_URL}search?sort=-date&model=article&page=${page}&per_page=${per_page}&`,
+            null,
+            query
+        );
     }
 
     getItemByQuery(query) {
         replacePolishAccents(query);
-        return this.fetch(`q=${query}&`);
-    }
-
-    paginate(page = 1, per_page = 10) {
-        return this.fetch(`page=${page}&per_page=${per_page}`);
+        return this.fetch(`q=${query}`);
     }
 
     filterItemsByModel(query) {
